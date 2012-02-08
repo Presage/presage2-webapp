7b4e5dc92a2d9516fba5abf9049796c72a3e1109
Sam Macbeth <sammacbeth@gmail.com>
2012-02-08 00:28:37 +0000

    Simulation details form.

--- src/main/webapp/app/view/SimulationsTable.js
+++ src/main/webapp/app/view/SimulationsTable.js
@@ -10,6 +10,12 @@ Ext.define('Presage2.view.SimulationsTable', {
 				return "--"
 			}
 		};
+		var formatDateField = function(field, newValue, oldValue) {
+			var timestamp = parseFloat(newValue);
+			if(!isNaN(timestamp) && isFinite(newValue)) {
+				field.setValue(formatDate(timestamp));
+			}
+		}
 
 		var grid = Ext.create('Ext.grid.Panel', {
 			store: 'Simulations',
@@ -65,14 +71,123 @@ Ext.define('Presage2.view.SimulationsTable', {
 			invalidateScrollerOnRefresh: false,
 			viewConfig: {
 				trackOver: false
+			},
+			listeners: {
+				selectionchange: function(model, records) {
+					if (records[0]) {
+						simDetails.getForm().loadRecord(records[0]);
+					}
+				}
 			}
-		})
+		});
+		
+		var simDetails = Ext.create('Ext.form.Panel', {
+			store: 'Simulations',
+			xtype: 'form',
+			title: 'Simulation Details',
+			region: 'south',
+			split: true,
+			frame: true,
+			animCollapse: true,
+			collapsible: true,
+			collapsed: true,
+			height: 200,
+			layout: {
+				type: 'hbox',
+				align: 'stretch'
+			},
+			fieldDefaults: {
+				labelAlign: 'left',
+				msgTarget: 'side'
+			},
+			items: [{
+				xtype: 'fieldset',
+				defaultType: 'textfield',
+				flex: 1,
+				bodyPadding: 5,
+				border: 0,
+				items: [{
+					fieldLabel: 'ID',
+					name: 'id',
+					disabled: true
+				},{
+					fieldLabel: 'Name',
+					name: 'name',
+					width: 400
+				},{
+					fieldLabel: 'Class name',
+					name: 'classname',
+					width: 500
+				},{
+					fieldLabel: 'State',
+					name: 'state',
+					xtype: 'combo',
+					allowBlank: false,
+					store: [
+						'LOADING',
+						'READY',
+						'INITIALISING',
+						'RUNNING',
+						'PAUSED',
+						'STOPPED',
+						'FINISHING',
+						'COMPLETE'
+					]
+				},{
+					fieldLabel: 'Current Time',
+					name: 'currentTime',
+					xtype: 'displayfield',
+					value: ''
+				},{
+					fieldLabel: 'Finish Time',
+					name: 'finishTime',
+					xtype: 'displayfield',
+					value: ''
+				}]
+			},{
+				xtype: 'fieldset',
+				defaultType: 'textfield',
+				maxHeight: 200,
+				flex: 1,
+				bodyPadding: 5,
+				border: 0,
+				items: [{
+					fieldLabel: 'Created',
+					name: 'createdAt',
+					xtype: 'displayfield',
+					value: '',
+					width: 300,
+					listeners: {
+						change: formatDateField
+					}
+				},{
+					fieldLabel: 'Started',
+					name: 'startedAt',
+					xtype: 'displayfield',
+					value: '',
+					width: 300,
+					listeners: {
+						change: formatDateField
+					}
+				},{
+					fieldLabel: 'Finished',
+					name: 'finishedAt',
+					xtype: 'displayfield',
+					value: '',
+					width: 300,
+					listeners: {
+						change: formatDateField
+					}
+				}]
+			}]
+		});
+
 		Ext.apply(this, {
 			id: "sims-table",
 			layout: 'border',
 			frame: true,
 			items: [
-				grid
+				grid, simDetails
 			]
 		});
 		this.callParent(arguments);
